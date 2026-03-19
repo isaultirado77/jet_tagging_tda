d9c3a713fd1ac59a5410d62ead953258ef5b8305
Isaul Tirado <tiradolisaul@gmail.com>
2026-03-19 13:31:20 -0700

    add path configurations for datasets and results directories

--- src/jet_tagging/config.py
+++ src/jet_tagging/config.py
@@ -3,12 +3,16 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parents[2]
 
 # Paths
+PLOTS_DIR = BASE_DIR / "plots"
+RESULTS_DIR = BASE_DIR / "results"
+LOGS_DIR = BASE_DIR / "logs"
 DATA_DIR = BASE_DIR / "data"
 RAW_DIR = DATA_DIR / "raw"
 JETS_RAW_DIR = RAW_DIR / "jets"
 PROCESSED_DIR = DATA_DIR / "processed"
 JETS_PROCESSED_DIR = PROCESSED_DIR / "jets"
 TOPOLOGY_DIR = DATA_DIR / "topology"
+MERGED_DATASETS_DIR = DATA_DIR / "merged"
 
 
 # helper functions
